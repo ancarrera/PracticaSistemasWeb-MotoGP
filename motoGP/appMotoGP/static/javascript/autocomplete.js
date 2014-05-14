e22fe0f41eeb24bd5ed0c67df3722d2394a9940e
ancarrera <ancarrera31@gmail.com>
2014-05-14 02:09:06 +0200

    add style and restructure jquery

---
+++ motoGP/appMotoGP/static/javascript/autocomplete.js
@@ -0,0 +1,44 @@
+//<!--
+ $(function() {
+            $.getJSON("/static/json/circuits.json", {}, function (circuits) {
+                $("#id_debut_circuit").autocomplete({
+                    source: circuits,
+                     minLength: 2,
+
+                });
+            });
+        });
+
+$(document).ready( function(){ 
+$("#id_representative_company").autocomplete({
+                source: function( request, response ) {
+                    $.ajax({
+                        type: 'GET',
+                        url: "http://api.climatecounts.org/1/Companies.json?Search="+request.term+"&IncludeBrands=True",
+                        dataType: "jsonp",
+
+                        data: {
+                            format:'json'
+                        },
+                        minLength: 2,
+                        success: function( data ) {
+                                   
+                            response( $.map( data.Companies, function( item ) {
+                               
+                                return {
+                                    label: item.Name, 
+                                    value: item.Name,
+                                }
+                            }));
+                        }
+                    });
+                },
+               
+                select: function( event, ui ) {
+                    if (ui.item) {
+                        $("#id_representative_company").val(ui.item.Name);
+                    }
+                }
+            });
+    });
+//-->
